2aa52db046034c88cfdc3479465af1cac9bb1541
porao <ratomorto@riseup.net>
2020-11-01 14:32:58 -0300

    more inserts

--- insert.sql
+++ insert.sql
@@ -41,72 +41,93 @@ INSERT INTO reconecta_pessoa
 VALUES (s_id_reconecta_pessoa.nextval, 'Robson', 519892545, 'robson@bol.com.br', 'Cidreira');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um computador funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 1, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 1, 1, 'Um notebook quebrado - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um computador funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 1, 1, 'Um notebook funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um computador funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 1, 0, 'Um computador quebrado');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 1, 1, 'Um notebook funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um computador funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_computador
-VALUES (s_id_reconecta_computador.nextval, 0, 1, 'Um notebook funcionando - 8gb de RAM - Dell')
+VALUES (s_id_reconecta_computador.nextval, 1, 1, 'Um notebook funcionando - 8gb de RAM - Dell');
 
 INSERT INTO reconecta_quero_doar 
 VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 2, 2, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 3, 3, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 4, 4, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 5, 5, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 6, 6, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 7, 7, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_doar 
-VALUES (s_id_reconecta_quero_doar.nextval, 1, 1, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_doar.nextval, 8, 8, TO_DATE('2020/07/09', 'yyyy/mm/dd'));
 
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/13', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 9, 1, 'Preciso de um notebook para estudar' ,TO_DATE('2020/08/12', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 3, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/09/03', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 14, 1, 'Preciso de um notebook para assistir netflix' ,TO_DATE('2020/05/20', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 16, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/10/10', 'yyyy/mm/dd'));
 
 INSERT INTO reconecta_quero_receber
-VALUES (s_id_reconecta_quero_receber.nextval, 11, 0, 'Preciso de um computador para estudar' ,TO_DATE('2020/07/10', 'yyyy/mm/dd'));
+VALUES (s_id_reconecta_quero_receber.nextval, 18, 1, 'Preciso de um notebook pra jogar paciencia' ,TO_DATE('2020/11/01', 'yyyy/mm/dd'));
+
+INSERT INTO reconecta_doacao 
+VALUES (s_id_reconecta_doacao.nextval, 1, 6, '13-jan-2020');
+
+INSERT INTO reconecta_doacao 
+VALUES(s_id_reconecta_doacao.nextval, 2, 5, '22-mar-2020');
+
+INSERT INTO reconecta_doacao 
+VALUES(s_id_reconecta_doacao.nextval, 7, 4, '07-nov-2020');
+
+INSERT INTO reconecta_doacao 
+VALUES(s_id_reconecta_doacao.nextval, 4, 1, '04-may-2020');
+
+INSERT INTO reconecta_doacao 
+VALUES(s_id_reconecta_doacao.nextval, 1, 2, '11-feb-2020');
+
+INSERT INTO reconecta_doacao 
+VALUES(s_id_reconecta_doacao.nextval, 6, 1, '28-jul-2020');
+
+
+
 
